1029de16dbf57caa658ecfff020109772361c433
RanaNitin02 <nitinrana0529@gmail.com>
2025-07-22 23:24:46 +0530

    configured clerk and created landing pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,34 @@
-// import { Button } from "@/components/ui/button"
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
+import PublicLayout from '@/layout/PublicLayout';
+import AuthLayout from '@/layout/AuthLayout';
+
+import HomePage from '@/routes/HomePage';
+import Login from '@/routes/Login';
+import Signup from '@/routes/Signup';
 
 const App = () => {
   return (
-    <div className='text-blue-500'>
-      App
-    </div>
+    <Router>
+      <Routes>
+
+        { /* public routes */ }
+        <Route element={<PublicLayout />}>
+          <Route index element={<HomePage />} />
+        </Route>
+
+        { /* authentication layout */ }
+        <Route element={<AuthLayout />}>
+          <Route path='/login' element={<Login />} />
+          <Route path='/signup' element={<Signup />} />
+        </Route>
+
+
+        { /* protected routes */ }
+
+
+      </Routes>
+    </Router>
   )
 }
 
---
+++ frontend/src/layout/AuthLayout.tsx
@@ -0,0 +1,14 @@
+import { Outlet } from "react-router-dom"
+
+
+const AuthLayout = () => {
+  return (
+    <div className="w-full">
+
+        <Outlet />
+        
+    </div>
+  )
+}
+
+export default AuthLayout
